6a3a719d5be3ec22bb24efbe5159375c163cae56
SkyHighSundae <156344110+SkyHighSundae@users.noreply.github.com>
2024-02-07 16:59:17 +0100

    Update not fork.py

--- not fork.py
+++ not fork.py
@@ -11,6 +11,16 @@ class TextColorDialog(Gtk.Dialog):
         self.get_content_area().add(self.color_chooser)
         self.show_all()
 
+class BugsDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, title="Known Bugs", transient_for=parent, flags=0)
+        self.add_buttons(Gtk.STOCK_OK, Gtk.ResponseType.OK)
+
+        label = Gtk.Label(label="Known Issue: You can only set the text color once per launch.")
+        box = self.get_content_area()
+        box.add(label)
+        self.show_all()
+
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Not Fork")
@@ -21,7 +31,7 @@ class MainWindow(Gtk.Window):
         self.textview = Gtk.TextView()
         self.textview.set_editable(False)  # Make the TextView not editable
         self.textbuffer = self.textview.get_buffer()
-        self.textbuffer.set_text("Not fork is a forkie in collabvm. He keeps rm - rf ing vms. He got banned from lubuntuvm by guest24897")
+        self.textbuffer.set_text("Not fork is a forkie in collabvm. he keeps rm - rf ing vms. he got banned from lubuntuvm by guest24897")
 
         # Scrollable window for the TextView
         scrollable_tview = Gtk.ScrolledWindow()
@@ -53,6 +63,11 @@ class MainWindow(Gtk.Window):
         text_color_item.connect("activate", self.open_color_dialog)
         tools_menu.append(text_color_item)
 
+        # Bugs menu item
+        bugs_item = Gtk.MenuItem(label="Bugs")
+        bugs_item.connect("activate", self.open_bugs_dialog)
+        tools_menu.append(bugs_item)
+
         menubar.append(file_item)
         menubar.append(tools_item)
 
@@ -73,6 +88,11 @@ class MainWindow(Gtk.Window):
             end_iter = self.textbuffer.get_end_iter()
             self.textbuffer.apply_tag(text_tag, start_iter, end_iter)
 
+        dialog.destroy()
+
+    def open_bugs_dialog(self, widget):
+        dialog = BugsDialog(self)
+        dialog.run()
         dialog.destroy()
 
 win = MainWindow()
